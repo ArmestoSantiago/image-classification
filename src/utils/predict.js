90c15ae630226932b64e234890b3fe200c4bef69
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-17 12:16:34 -0300

    update

--- src/utils/predict.js
+++ src/utils/predict.js
@@ -1,9 +1,19 @@
-import { model } from './index.js';
 import { imageProcess } from './imageProcess.js';
+import * as tf from '@tensorflow/tfjs';
+
 
 const classes = ['Gato', 'Perro'];
 
+let model;
+
+export async function loadModel() {
+  model = await tf.loadLayersModel('model/model.json');
+  console.log('Modelo cargado');
+}
+
+
 export async function predict(imageElement) {
+  await loadModel();
   if (!model) {
     return null;
   }
