6a4f55f8e0b9568b3ff206122c145595df0445e6
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-17 12:24:25 -0300

    update version test

--- src/classification.d.ts
+++ src/classification.d.ts
@@ -1,4 +1,5 @@
 declare module 'sos-image-classification' {
   export function predict(imageElement: HTMLImageElement): Promise<string>;
   export function loadModel(): Promise<any>;
+  export const se: string;
 }
