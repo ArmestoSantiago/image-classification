f17a5bd676cbff482dbf6b6aa780a449bb695af2
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-17 12:16:58 -0300

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
-import * as tf from '@tensorflow/tfjs';
-
-export let model = null;
-
-async function loadModel() {
-  model = await tf.loadLayersModel('./model/model.json');
-  console.log('Modelo cargado automáticamente');
-}
-
-loadModel();
-
 export { predict } from './utils/predict';
