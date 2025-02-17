c1f0a514a49d9e8d4a666028bae7766382384ed1
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-17 11:08:23 -0300

    first commit

--- src/utils/model.js
+++ src/utils/model.js
@@ -3,7 +3,7 @@ import * as tf from '@tensorflow/tfjs';
 let model;
 
 export async function loadModel() {
-  model = await tf.loadLayersModel('./model/model.json');
+  model = await tf.loadLayersModel('model/model.json');
   console.log('Modelo cargado');
 }
 
