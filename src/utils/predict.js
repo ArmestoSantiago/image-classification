3dda1c479785338c3494dd99f946f7383604ba4a
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-17 23:53:41 -0300

    update

--- src/utils/predict.js
+++ src/utils/predict.js
@@ -7,8 +7,8 @@ const classes = ['Gato', 'Perro'];
 let model;
 
 export async function loadModel() {
-  console.log(model);
-  model = await tf.loadLayersModel('src/utils/model.json');
+  const modelURL = new URL('../model/model.json', import.meta.url).href;
+  model = await tf.loadLayersModel(modelURL);
   return model;
 }
 
